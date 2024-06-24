425a2fbccd6fa936364b06a3aac24e0caca38f19
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-06-23 23:02:25 -0300

    update de una opcion

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -28,10 +29,10 @@ export class CarsController {
 
     @Patch(':id')
     updateCar(
-        @Param('id', ParseIntPipe) id: number,
-        @Body() body: any)
+        @Param('id', ParseUUIDPipe) id: string,
+        @Body() updateCarDto: UpdateCarDto)
         {
-        return body;
+        return this.carService.update(id, updateCarDto);
     }
 
     @Delete(':id')
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,9 +1,9 @@
-import { Get, Injectable, NotFoundException, Param } from '@nestjs/common';
+import { BadRequestException, Get, Injectable, NotFoundException, Param } from '@nestjs/common';
 
 import { v4 as uuid } from 'uuid';
 
 import { Car } from './interfaces/car.interface';
-import { CreateCarDto } from './dto/create-car.dto';
+import { CreateCarDto,UpdateCarDto } from './dto'
 
 @Injectable()
 export class CarsService {
@@ -48,5 +48,24 @@ export class CarsService {
     this.cars.push(newCar);
 
     return newCar;
+  }
+
+  update(id: string, updateCarDto: UpdateCarDto) {
+
+    let carDB = this.findById(id);
+
+    if(updateCarDto.id && updateCarDto.id !== id) throw new BadRequestException(`Car with id ${id} is not valid inside body update`);
+
+    this.cars = this.cars.map( car => {
+      if (car.id === id) {
+        carDB = {...carDB,...updateCarDto,id};
+
+        return carDB;
+      } else {
+        return car;
+      }
+    })
+
+    return carDB;
   }
 }
